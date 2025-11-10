216995d1787c341b19570f9a72e23bc45d9acf0e
Stephen <159204715+StephenK0@users.noreply.github.com>
2025-11-10 16:18:08 -0500

    Add a mostly-complete Interaction for entering a fight, as well as a relevant test scene

---
+++ Assets/Interacting/Interactions/EnterFight.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+//  Opens a UI panel for selecting the cards for a fight, 
+//  begins an Inventory card selection, 
+//  displays a card's description when relevant, 
+//  displays how many cards are selected and how many are necessary, 
+//  checks whether the correct number of cards are selected, 
+//  waits for the user to press a button while the right number of cards are selected, 
+//  and reaps the cards selected by the user, allowing them to be used by another script.
+//
+//  TODO: Make it so that certain fields that are reused a lot are stored statically. 
+public class EnterFight : Interaction
+{
+	//Represents the panel on which the ui for selecting cards is. 
+	[SerializeField] private protected GameObject uiPanel;
+
+	//Represents the textbox on which the card description should be printed. 
+	[SerializeField] private protected TMP_Text description;
+
+	//Represents the textbox for displaying number of cards required for this fight. 
+	[SerializeField] private protected TMP_Text count;
+
+	//How many cards are required for this fight. An exact number, not a range or a minimum. 
+	[SerializeField] private protected int howManyCardsToChoose;
+
+	//The Inventory responsible for selecting cards. 
+	[SerializeField] private protected Inventory inv;
+
+
+	bool ready = false;
+
+	private protected void Awake(){
+		uiPanel.SetActive(false);
+	}
+
+	private protected override void StuffToDo(){
+		uiPanel.SetActive(true);
+		inv.StartSelection();
+		ready = true;
+	}
+
+	private protected void Update(){
+		if(ready){
+			count.text = inv.GetCount() + "/" + howManyCardsToChoose;
+			if(inv.GetCount() == howManyCardsToChoose){
+				count.text += "\nPress space to begin!";
+				if(Input.GetKeyDown("space") || Input.GetKeyDown("enter")){
+					inv.ReapCards(); 
+					//TODO!!
+					Debug.LogError("Here, a new scene would begin for a card battle, but that functionality has not yet been added!");
+				}
+			}
+		}
+	}
+}
